a4370ac79a4b7309d2f5e2ac22fb749ef5ad21d6
Anders Johansson <trumanders@gmail.com>
2023-04-24 13:56:10 +0200

    en annan navbar-design

--- src/assets/components/navBar/NavBar.jsx
+++ src/assets/components/navBar/NavBar.jsx
@@ -2,52 +2,61 @@ import { useState, useEffect } from "react";
 import "./NavBar.css";
 import "./NavFunc";
 import { Burger } from "./burger.jsx";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-function NavBar(){
-    const [visible, setVisible] = useState(false)
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 730);
+function NavBar() {
+  const [visible, setVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 730);
 
-    const updateMedia = () => {
-        setIsMobile(window.innerWidth < 730)
-    }
+  const updateMedia = () => {
+    setIsMobile(window.innerWidth < 730);
+  };
 
-    useEffect(() => {
-        window.addEventListener("resize", updateMedia);
-        return () => window.removeEventListener("resize", updateMedia);
-    }, [isMobile]);
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, [isMobile]);
 
   return (
     <div className="NavBar">
-        <a href="./"><img src="#" className="Logotype" alt="Logotype"/></a>
-        <div className="NaviIntro">
-            <button onClick={() => setVisible(!visible)}><Burger></Burger></button>
-            <p>Nail Soup</p>
-            <div className="searchIcon">
-                <img src="src/assets/components/navBar/pngegg.png" alt="Search recipe" />
-            </div>
+      <a href="./">
+        <img src="#" className="Logotype" alt="Logotype" />
+      </a>
+      <div className="NaviIntro">
+        <button onClick={() => setVisible(!visible)}>
+          <Burger></Burger>
+        </button>
+        <p>Nail Soup</p>
+        <div className="searchIcon">
+          <img
+            src="src/assets/components/navBar/pngegg.png"
+            alt="Search recipe"
+          />
         </div>
-            {!isMobile || visible ? (
+      </div>
+      {!isMobile || visible ? (
         <ul>
           <li className="navOpt">
-            <Link to="/">
+            <NavLink to="/">
               <p>Home</p>
-            </Link>
+            </NavLink>
           </li>
           <li className="navOpt">
-            <Link to="/about">
+            <NavLink to="/about">
               <p>About</p>
-            </Link>
+            </NavLink>
           </li>
           <li className="navOpt">
-            <Link to="/contact">
+            <NavLink to="/contact">
               <p>Contact</p>
-            </Link>
+            </NavLink>
           </li>
         </ul>
-        ) : (<></>)}
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
 
-export {NavBar};
+export { NavBar };
